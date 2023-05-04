c927b4d1027ddec98ba0070c3e975033268f5bdc
Eric Hartline <hartline.eric@gmail.com>
2023-05-03 19:19:52 -0500

    add websockets and royal sasquatch image

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const http = require('http');
+const http = require("http");
 
-http.createServer(function (req, res) {
-res.write("On the way to being a full stack engineer!")
-res.end()
-}
-).listen(3000)
+http
+  .createServer(function (req, res) {
+    res.write("Hello world!");
+    res.end();
+  })
+  .listen(3000);
 
-console.log('server started')
-
+console.log("server started");
